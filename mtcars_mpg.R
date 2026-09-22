111360509b65801235752d234b1c2b0947e40c80
savjmccoy <smccoy@student.ubc.ca>
2026-09-22 10:00:17 -0700

    added mtcars_mpg.R

---
+++ mtcars_mpg.R
@@ -0,0 +1,9 @@
+#view the first few rowws of mtcars dataset
+
+head(mtcars)
+
+#calculate the avg mpg among all cars and save it as a variable
+mean_mpg <- mean(mtcars$mpg)
+
+#print out the mean mpg
+mean_mpg
